a3ad98f13f10ec28f4edcb2abd9e1100f803d134
Pradyoth P <pradyoth.jpg@gmail.com>
2022-07-12 14:02:13 +0530

    rebase

--- WebApp/app.py
+++ WebApp/app.py
@@ -2,8 +2,10 @@ from flask import Flask,render_template,url_for,request
 import pickle
 import numpy as np
 import re
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.feature_extraction.text import TfidfVectorizer
 import pandas as pd
+from sklearn.naive_bayes import MultinomialNB
 import tweepy
 from tweepy import OAuthHandler 
 # consumer_key="nKYTHw8FJ45dtxKbnURY7Nvuz"
@@ -38,7 +40,7 @@ def predict():
         except tweepy.errors.TweepError as e:
             print(f"Error: Tweeter Authentication Failed - \n{str(e)}")
 
-    def get_tweets(self, query, maxTweets = 1000):
+    def get_tweets(self, query, maxTweets = 100):
         #Function to fetch tweets. 
         # empty list to store parsed tweets 
         tweets = [] 
@@ -115,18 +117,19 @@ def predict():
   d.head()
   x = d.iloc[:,-2].values
   d.head()
-  tv = TfidfVectorizer(max_df=0.90, min_df=2, stop_words='english',ngram_range=(1, 2),max_features=6000)
+  tv = TfidfVectorizer(max_df=0.90, min_df=2, stop_words='english',ngram_range=(1, 2),max_features=23252)
   print(tv)
   x = tv.fit_transform(x.astype('U'))
-  print(tv)
-  pickle_in = open("App.pickle","rb") 
+#   print(tv.transform([tweet]).toarray())
+#   pickle_in = open("App.pickle","rb") 
+  pickle_in= open("randomforest.pickle","rb")
   classifier1 = pickle.load(pickle_in)
 #   classifier1.fit()
   print(classifier1)
   if request.method == 'POST':
     comment = request.form['Tweet']
     twitter_client = TwitterClient()
-    tweets_df = twitter_client.get_tweets(comment, maxTweets=100)                           
+    tweets_df = twitter_client.get_tweets(comment, maxTweets=1000)                           
     tweets_df['len']=tweets_df["tweets"].str.len()                       
     df1= tweets_df[(tweets_df['len'] <=137)]
     df2=tweets_df[(tweets_df['len'] >=150)]
@@ -138,32 +141,34 @@ def predict():
     ops = []
     for i,tweet in enumerate(data['clean']):
       print(tv.transform([tweet]).toarray())
-      op=classifier1.predict(tv.transform([tweet]).toarray())
+      X=tv.transform([tweet]).toarray()
+      op=classifier1.predict(X)
       if op == [0]:
         tweets.append(data.tweets[i])
         ops.append('Negative') 
       if op == [1]:
         tweets.append(data.tweets[i])
-        ops.append('Neutral')
+        ops.append('Positive')
       if op == [2]:
         tweets.append(data.tweets[i])
-        ops.append('Positive')
+        ops.append('Neutral')
     output=dict(zip(tweets,ops))
     Neucount = ops.count('Neutral')
     Negcount = ops.count('Negative')
     Poscount = ops.count('Positive')
-    emo=con1(data['clean'].sum())
-    h=emo.count(' happy')
-    s=emo.count(' sad')
-    a=emo.count(' angry')
-    l=emo.count(' loved')
-    pl=emo.count(' powerless')
-    su=emo.count(' surprise')
-    fl=emo.count(' fearless')
-    c=emo.count(' cheated')
-    at=emo.count(' attracted')
-    so=emo.count(' singled out')
-    ax=emo.count(' anxious')
-    return render_template('result.html',outputs = output,NU=Neucount,N=Negcount,P=Poscount,happy=h,sad=s,angry=a,loved=l,powerless=pl,surprise=su,fearless=fl,cheated=c,attracted=at,singledout=so,anxious=ax)
+    # emo=con1(data['clean'].sum())
+    # h=emo.count(' happy')
+    # s=emo.count(' sad')
+    # a=emo.count(' angry')
+    # l=emo.count(' loved')
+    # pl=emo.count(' powerless')
+    # su=emo.count(' surprise')
+    # fl=emo.count(' fearless')
+    # c=emo.count(' cheated')
+    # at=emo.count(' attracted')
+    # so=emo.count(' singled out')
+    # ax=emo.count(' anxious')
+    # return render_template('result.html',outputs = output,NU=Neucount,N=Negcount,P=Poscount,happy=h,sad=s,angry=a,loved=l,powerless=pl,surprise=su,fearless=fl,cheated=c,attracted=at,singledout=so,anxious=ax)
+    return render_template('result.html',outputs = output,NU=Neucount,N=Negcount,P=Poscount)
 if __name__ == '__main__':
 	app.run(debug=True, use_reloader=False)
